fbabe1478772655fea2d121570d36a01b39d864f
apostoliseq <apostolisn@hotmail.com>
2025-05-29 16:38:18 +0000

    Add endpoint to fetch db entry

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ms-nosql
+
+## Create a Docker network so containers can communicate
+docker network create test-network
--- microservices/python/app.py
+++ microservices/python/app.py
@@ -3,10 +3,18 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb://test-container-mongo:27017/')
+db = client['test']
+collection = db['coll']
+
 @app.route('/test-python')
 def hello_world():
-
     return "Hello World!"
+
+@app.route('/test-get')
+def get_data():
+    result = collection.find_one({"name": "Max"})
+    return str(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
